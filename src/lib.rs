f3509ba6a76633bb2386274c29b7ce97a56077ad
zayn-kahn <khanzain101325@gmail.com>
2022-06-23 12:21:04 +0500

    logic

--- src/lib.rs
+++ src/lib.rs
@@ -7,19 +7,63 @@ pub use pallet::*;
 pub mod pallet {
 	use frame_support::pallet_prelude::*;
 	use frame_system::pallet_prelude::*;
-	use sp_std::vec::Vec; // Step 3.1 will include this in `Cargo.toml`
+	use sp_std::vec::Vec;
 
-	#[pallet::config]  // <-- Step 2. code block will replace this.
-	#[pallet::event]   // <-- Step 3. code block will replace this.
-	#[pallet::error]   // <-- Step 4. code block will replace this.
+#[pallet::config]
+pub trait Config: frame_system::Config {
+	type Event: From<Event<Self>> + IsType<<Self as frame_system::Config>::Event>;
+}
+#[pallet::event]
+#[pallet::generate_deposit(pub(super) fn deposit_event)]
+pub enum Event<T: Config> {
+	ClaimCreated(T::AccountId, Vec<u8>),
+	ClaimRevoked(T::AccountId, Vec<u8>),
+}
+#[pallet::error]
+pub enum Error<T> {
+	ProofAlreadyClaimed,
+	NoSuchProof,
+	NotProofOwner,
+}
 	#[pallet::pallet]
 	#[pallet::generate_store(pub(super) trait Store)]
 	#[pallet::generate_storage_info]
 	pub struct Pallet<T>(_);
 
-	#[pallet::storage] // <-- Step 5. code block will replace this.
+#[pallet::storage]
+pub(super) type Proofs<T: Config> = StorageMap<_, Blake2_128Concat, Vec<u8>, (T::AccountId, T::BlockNumber), ValueQuery>;
 	#[pallet::hooks]
 	impl<T: Config> Hooks<BlockNumberFor<T>> for Pallet<T> {}
-	#[pallet::call]   // <-- Step 6. code block will replace this.
+
+#[pallet::call]
+impl<T: Config> Pallet<T> {
+	#[pallet::weight(1_000)]
+	pub fn create_claim(
+		origin: OriginFor<T>,
+		proof: Vec<u8>,
+	) -> DispatchResult {
+		let sender = ensure_signed(origin)?;
+		ensure!(!Proofs::<T>::contains_key(&proof), Error::<T>::ProofAlreadyClaimed);
+		let current_block = <frame_system::Pallet<T>>::block_number();
+		Proofs::<T>::insert(&proof, (&sender, current_block));
+		Self::deposit_event(Event::ClaimCreated(sender, proof));
+
+		Ok(())
+	}
+
+	#[pallet::weight(10_000)]
+	pub fn revoke_claim(
+		origin: OriginFor<T>,
+		proof: Vec<u8>,
+	) -> DispatchResult {
+		let sender = ensure_signed(origin)?;
+		ensure!(Proofs::<T>::contains_key(&proof), Error::<T>::NoSuchProof);
+		let (owner, _) = Proofs::<T>::get(&proof);
+		ensure!(sender == owner, Error::<T>::NotProofOwner);
+		Proofs::<T>::remove(&proof);
+		Self::deposit_event(Event::ClaimRevoked(sender, proof));
+		Ok(())
+	}
+}
 }
 
